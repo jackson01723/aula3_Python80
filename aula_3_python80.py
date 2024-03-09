a30ad06d1c03a08151b7d66d2475047be7a14c26
jackson01723 <156862370+jackson01723@users.noreply.github.com>
2024-03-09 17:48:09 -0300

    Add files via upload

---
+++ aula_3_python80.py
@@ -0,0 +1,26 @@
+#variável do tipo real-> float
+
+#real = 1.5
+#print(real)
+
+#variável do tipo texto
+
+#texto = 'isso é um texto'
+#print(texto)
+
+#variável do tipo boleano #1 V #0 F
+
+#booleano1 = True
+#booleano2 = False
+
+#vareável do tipo inteiro
+#numero_inteiro = 10
+
+#concatenação -> juntar as informaçães de output
+
+#nome = 'Fernando'
+#dade = 18
+#endereco =  'rua 7'
+#print('Nome:',nome)
+#print('Idade:',idade)
+#print('Endereço:',endereco)
